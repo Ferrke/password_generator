d4b8acecc5a2011a9a085f7533c59a32925d3ea2
Ferrke <ferrke@proton.me>
2023-03-25 21:51:15 +0100

    Random password generator

---
+++ pass.py
@@ -0,0 +1,14 @@
+import string
+
+def generate_password(length):
+    # Define the set of characters to be used in the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate the password 
+    password = ''.join(secrets.choice(characters, k = length))
+
+    return password
+
+number = input("How long do you want your password to be?\ntype a number between 1 and and a 1000 :")
+password = generate_password(int(number))
+print(password)
